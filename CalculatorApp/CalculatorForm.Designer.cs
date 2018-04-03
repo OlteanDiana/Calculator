296245fe391f2718c33a9e7a6dd0ef9a3e413683
Oltean <Oltean@DIANA>
2018-04-03 23:55:34 +0300

    Add for positive numbers

--- CalculatorApp/CalculatorForm.Designer.cs
+++ CalculatorApp/CalculatorForm.Designer.cs
@@ -28,20 +28,73 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.buttonAdd = new System.Windows.Forms.Button();
+            this.btnSelectFiles = new System.Windows.Forms.Button();
+            this.openFileDialog = new System.Windows.Forms.OpenFileDialog();
+            this.textBoxResult = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
+            // 
+            // buttonAdd
+            // 
+            this.buttonAdd.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.buttonAdd.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.buttonAdd.Location = new System.Drawing.Point(12, 98);
+            this.buttonAdd.Name = "buttonAdd";
+            this.buttonAdd.Size = new System.Drawing.Size(75, 73);
+            this.buttonAdd.TabIndex = 0;
+            this.buttonAdd.Text = "+";
+            this.buttonAdd.UseVisualStyleBackColor = true;
+            this.buttonAdd.Click += new System.EventHandler(this.buttonAdd_Click);
+            // 
+            // btnSelectFiles
+            // 
+            this.btnSelectFiles.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.btnSelectFiles.Location = new System.Drawing.Point(12, 24);
+            this.btnSelectFiles.Name = "btnSelectFiles";
+            this.btnSelectFiles.Size = new System.Drawing.Size(145, 42);
+            this.btnSelectFiles.TabIndex = 2;
+            this.btnSelectFiles.Text = "Select files";
+            this.btnSelectFiles.UseVisualStyleBackColor = true;
+            this.btnSelectFiles.Click += new System.EventHandler(this.btnSelectFiles_Click);
+            // 
+            // openFileDialog
+            // 
+            this.openFileDialog.DefaultExt = "txt";
+            this.openFileDialog.FileName = "openFileDialog";
+            this.openFileDialog.Filter = "txt files (*.txt)|*.txt";
+            this.openFileDialog.FilterIndex = 2;
+            this.openFileDialog.Multiselect = true;
+            this.openFileDialog.RestoreDirectory = true;
+            // 
+            // textBoxResult
+            // 
+            this.textBoxResult.AcceptsReturn = true;
+            this.textBoxResult.Location = new System.Drawing.Point(12, 201);
+            this.textBoxResult.Multiline = true;
+            this.textBoxResult.Name = "textBoxResult";
+            this.textBoxResult.Size = new System.Drawing.Size(666, 192);
+            this.textBoxResult.TabIndex = 3;
             // 
             // CalculatorForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(282, 253);
+            this.ClientSize = new System.Drawing.Size(690, 405);
+            this.Controls.Add(this.textBoxResult);
+            this.Controls.Add(this.btnSelectFiles);
+            this.Controls.Add(this.buttonAdd);
             this.Name = "CalculatorForm";
             this.Text = "Calculator";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+        private System.Windows.Forms.Button buttonAdd;
+        private System.Windows.Forms.Button btnSelectFiles;
+        private System.Windows.Forms.OpenFileDialog openFileDialog;
+        private System.Windows.Forms.TextBox textBoxResult;
     }
 }
 
--- CalculatorApp/CalculatorForm.cs
+++ CalculatorApp/CalculatorForm.cs
@@ -1,20 +1,55 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace CalculatorApp
 {
     public partial class CalculatorForm : Form
     {
+        private string _firstFilePath;
+        private string _secondFilePath;
+        private bool _filesImported;
+        private AddHandler _addHandler;
+
         public CalculatorForm()
         {
             InitializeComponent();
+            _addHandler = new AddHandler();
+        }
+
+        private void btnSelectFiles_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog.ShowDialog() != DialogResult.OK)
+            {
+                return;
+            }
+
+            if (openFileDialog.FileNames.Length != 2)
+            {
+                MessageBox.Show("Please select two text files!");
+                return;
+            }
+
+            _firstFilePath = openFileDialog.FileNames[0];
+            _secondFilePath = openFileDialog.FileNames[1];
+            _filesImported = true;
+        }
+
+        private void buttonAdd_Click(object sender, EventArgs e)
+        {
+            if (!_filesImported)
+            {
+                MessageBox.Show("Please import files first.");
+                return;
+            }
+
+            try
+            {
+                textBoxResult.Text = _addHandler.Add(_firstFilePath, _secondFilePath);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
         }
     }
 }
